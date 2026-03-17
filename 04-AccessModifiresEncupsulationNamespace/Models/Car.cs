095fa2531c1c50d94d749f50b18549ac95a4ec81
EchosDev <elmirfsh-apa202@code.edu.az>
2026-03-17 21:52:52 +0400

    Task 4 yazildi

---
+++ 04-AccessModifiresEncupsulationNamespace/Models/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_AccessModifiresEncupsulationNamespace.Models
+{
+    internal class Car : Vehicle
+    {
+        private double _fuelCapacity;
+        private double _fuel;
+        public double FuelConsumptionPer100Km { get; set; }
+        public double FuelCapacity
+        {
+            get { return _fuelCapacity; }
+            set
+            {
+                if (value > 0)
+                    _fuelCapacity = value;
+                else
+                    Console.WriteLine("Benzin tutumu 0 dan boyuk olmalidir");
+            }
+        }
+        public double Fuel
+        {
+            get { return _fuel; }
+            set
+            {
+                if (value > 0 && value <= FuelCapacity)
+                    _fuel = value;
+                else
+                    Console.WriteLine("Benzin 0 dan boyuk olmalidir ve Benzin benzin tutumundan kicikdir ve ya beraberolmalidir");
+            }
+        }
+        public Car(string brand, string model, int year, double fuelCapacity, double fuel, double fuelConsumptionPer100Km) : base(brand, model, year)
+        {
+            FuelCapacity = fuelCapacity;
+            Fuel = fuel;
+            FuelConsumptionPer100Km = fuelConsumptionPer100Km;
+        }
+        public void Refuel(double liters)
+        {
+            if (Fuel + liters <= FuelCapacity)
+            {
+                Fuel += liters;
+                Console.WriteLine($"Benzin dolduruldu hazirki benzin: {Fuel}");
+            }
+            else
+            {
+                Console.WriteLine("Benzin tutumu asilir");
+            }
+        }
+    }
+}
---
+++ 04-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,19 @@
+﻿using _04_AccessModifiresEncupsulationNamespace.Models;
+
+namespace _04_AccessModifiresEncupsulationNamespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car vehicle1 = new Car("Toyota", "Camry", 2020, 50, 10, 0);
+
+            vehicle1.StartEngine();
+            vehicle1.Drive(150);
+            vehicle1.VehicleInfo();
+            vehicle1.StopEngine();
+
+            vehicle1.Refuel(40);
+        }
+    }
+}
