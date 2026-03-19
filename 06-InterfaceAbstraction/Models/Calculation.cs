8b827b57b485e8518f8d23c499bd37ec3cf02f38
EchosDev <elmirfsh-apa202@code.edu.az>
2026-03-19 16:13:55 +0400

    New task 6 added

---
+++ 06-InterfaceAbstraction/Models/Calculation.cs
@@ -0,0 +1,36 @@
+﻿using _06_InterfaceAbstraction.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_InterfaceAbstraction.Models
+{
+    class Calculation : ICalculation
+    {
+        public double Calculate(double a, double b, char symbol)
+        {
+            switch (symbol)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b == 0)
+                    {
+                        Console.WriteLine("0-a bölmək olmaz!");
+                        return 0;
+                    }
+                    return a / b;
+
+                default:
+                    Console.WriteLine("Yansil emeliyat daxil etmisiniz");
+                    return 0;
+            }
+        }
+    }
+}
---
+++ 06-InterfaceAbstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using _06_InterfaceAbstraction.Models;
+
+namespace _06_InterfaceAbstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calc = new();
+
+            Console.WriteLine("Birinci ededi daxil edin:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Emeliyati daxil edin:");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine($"Netice: {calc.Calculate(a, b, symbol)}");
+        }
+    }
+}
