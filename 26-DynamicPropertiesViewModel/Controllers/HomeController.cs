d7f685ae5c42d7192a22211d034f74188d9732a5
EchosDev <elmirfsh-apa202@code.edu.az>
2026-05-05 20:13:07 +0400

    Task 26 added

---
+++ 26-DynamicPropertiesViewModel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using _26_DynamicPropertiesViewModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _26_DynamicPropertiesViewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Student> _students = new List<Student>
+        {
+            new Student { Id = 1, Name = "John", LastName = "Doe" },
+            new Student { Id = 2, Name = "Jane", LastName = "Smith" },
+        };
+        private List<Teacher> _teachers = new List<Teacher>
+        {
+            new Teacher { Id = 1, Name = "Mr. Anderson", Salary = 50000 },
+            new Teacher { Id = 2, Name = "Ms. Johnson", Salary = 55000 },
+        };
+
+        public IActionResult Index()
+        {
+            var homeVM = new ViewModel.HomeVM
+            {
+                Students = _students,
+                Teachers = _teachers
+            };  
+
+            return View(homeVM);
+        }
+
+        [Route("koperativ-satislar")]
+        public IActionResult CorporativeSales()
+        {
+            return View();
+        }
+    }
+}
